8c05c3232f66a3fa7809290e152497203024a3d7
Johan Pacheco <johanpach9@gmail.com>
2025-11-29 16:04:05 -0500

    'hptascampesion'

---
+++ browser-wasm/publish/wwwroot/Js/Home.js
@@ -0,0 +1,12 @@
+﻿window.initializeSwiper = () => {
+    console.log("Inicializando Swiper...");
+    setTimeout(() => {
+        new Swiper('.hero-swiper', {
+            loop: true,
+            autoplay: { delay: 3000, disableOnInteraction: false },
+            pagination: { el: '.swiper-pagination', clickable: true },
+            navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+            effect: 'fade'
+        });
+    }, 100); // 100ms de espera
+};
